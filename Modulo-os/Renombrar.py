ff06088b5a2131abe139205f7fb75d9fe14e1379
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-08-03 19:10:15 -0300

    metodos para renombrar archivos con os y pathlib

---
+++ Modulo-os/Renombrar.py
@@ -0,0 +1,15 @@
+
+
+import os
+from pathlib import Path
+
+# 5. Renombrar archivos con OS
+
+""" os.rename("Dataset", "Data") """
+
+# Renombrar con path
+
+path_actual = Path("Data")
+path_objetivo = Path("Dataset")
+
+Path.rename(path_actual, path_objetivo)
